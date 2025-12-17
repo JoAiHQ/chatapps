1155acaf3e1e82f30f7234932a7b6f58e724bdee
Micha Vie <micha@vleap.ai>
2025-12-17 14:01:31 +0100

    add debugging contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Alert } from '@openai/apps-sdk-ui/components/Alert'
+import { Button } from '@openai/apps-sdk-ui/components/Button'
+
 export function App() {
+  console.log('JoAi ChatGPT App: ', (window as any)?.openai)
   return (
     <div>
       <h1>Hello World</h1>
+      <Alert
+        actions={
+          <Button color="primary" pill variant="soft">
+            Dismiss
+          </Button>
+        }
+        title="Hello World"
+        description="This app is in progress."
+      />
     </div>
   )
 }
