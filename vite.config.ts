bcb95adcba43c0617daa6dfcc9ac861c0bb43c1f
Micha Vie <micha@vleap.ai>
2025-12-17 14:23:15 +0100

    upgrade deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: (ctx) => {
+      if (ctx.file?.includes('node_modules') && ctx.file?.includes('.module.css')) {
+        return {
+          plugins: [],
+        }
+      }
+      return {
+        plugins: {
+          '@tailwindcss/postcss': {},
+          autoprefixer: {},
+        },
+      }
+    },
+  },
 })
