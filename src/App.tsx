d1c65a18c476aa4ab6b9e7ad4bc73c11d79cf152
Micha Vie <micha@vleap.ai>
2025-12-18 12:50:34 +0100

    update content - testing

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { Button } from '@openai/apps-sdk-ui/components/Button'
 
 export function App() {
   console.log('JoAi ChatGPT App: ', (window as any)?.openai)
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex flex-col gap-4 p-4 max-w-lg mx-auto">
+      <h1 className="text-2xl font-bold">Hello World</h1>
       <Alert
         actions={
           <Button color="primary" pill variant="soft">
