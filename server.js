adff6a3bc5aee1216eca06dad611c33e464c28fb
Micha Vie <micha@vleap.ai>
2026-01-10 11:44:44 +0100

    update dev server port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3020
 const distDir = join(__dirname, 'dist')
 const devMode = process.env.NODE_ENV !== 'production'
 
